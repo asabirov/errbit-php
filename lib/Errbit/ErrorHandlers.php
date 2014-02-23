18922259ab0d9c7ee92c6ef81b93119c30eb3f6b
a.sabirov <a.sabirov@fun-box.ru>
2014-02-23 18:23:56 +0400

    Chaining errorHandler

--- lib/Errbit/ErrorHandlers.php
+++ lib/Errbit/ErrorHandlers.php
@@ -13,84 +13,90 @@
  * You can use your own, if you prefer to do so.
  */
 class Errbit_ErrorHandlers {
-	/**
-	 * Register all error handlers for the given $errbit client.
-	 *
-	 * @param [Errbit] $errbit
-	 *   the client instance
-	 *
-	 * @param [Array] $handlers
-	 *   an array of handler names, instead of registering all
-	 */
-	public static function register($errbit, $handlers = array('exception', 'error', 'fatal')) {
-		new self($errbit, $handlers);
-	}
+    /**
+     * Register all error handlers for the given $errbit client.
+     *
+     * @param [Errbit] $errbit
+     *   the client instance
+     *
+     * @param [Array] $handlers
+     *   an array of handler names, instead of registering all
+     */
+    public static function register($errbit, $handlers = array('exception', 'error', 'fatal')) {
+        new self($errbit, $handlers);
+    }
 
-	private $_errbit;
+    private $_errbit;
 
-	private $_prevExceptionHandler;
+    private $_prevExceptionHandler;
 
-	/**
-	 * Instantiate a new handler for the given client.
-	 *
-	 * @param [Errbit] $errbit
-	 *   the client to use
-	 */
-	public function __construct($errbit, $handlers) {
-		$this->_errbit = $errbit;
-		$this->_install($handlers);
-	}
+    private $_prevErrorHandler;
 
-	// -- Handlers
+    /**
+     * Instantiate a new handler for the given client.
+     *
+     * @param [Errbit] $errbit
+     *   the client to use
+     */
+    public function __construct($errbit, $handlers) {
+        $this->_errbit = $errbit;
+        $this->_install($handlers);
+    }
 
-	public function onError($code, $message, $file, $line) {
-		switch ($code) {
-			case E_NOTICE:
-			case E_USER_NOTICE:
-				$exception = new Errbit_Errors_Notice($message, $file, $line, debug_backtrace());
-				break;
+    // -- Handlers
 
-			case E_WARNING:
-			case E_USER_WARNING:
-				$exception = new Errbit_Errors_Warning($message, $file, $line, debug_backtrace());
-				break;
+    public function onError($code, $message, $file, $line) {
+        switch ($code) {
+            case E_NOTICE:
+            case E_USER_NOTICE:
+                $exception = new Errbit_Errors_Notice($message, $file, $line, debug_backtrace());
+                break;
 
-			case E_ERROR:
-			case E_USER_ERROR:
-			default:
-				$exception = new Errbit_Errors_Error($message, $file, $line, debug_backtrace());
-		}
+            case E_WARNING:
+            case E_USER_WARNING:
+                $exception = new Errbit_Errors_Warning($message, $file, $line, debug_backtrace());
+                break;
 
-		$this->_errbit->notify($exception);
-	}
+            case E_ERROR:
+            case E_USER_ERROR:
+            default:
+                $exception = new Errbit_Errors_Error($message, $file, $line, debug_backtrace());
+        }
 
-	public function onException($exception) {
-		$this->_errbit->notify($exception);
+        $this->_errbit->notify($exception);
 
-		if ($this->_prevExceptionHandler) {
-			call_user_func($this->_prevExceptionHandler, $exception);
-		}
-	}
+        if ($this->_prevErrorHandler) {
+            call_user_func($this->_prevErrorHandler, $code, $message, $file, $line);
+        }
+    }
 
-	public function onShutdown() {
-		if (($error = error_get_last()) && $error['type'] & error_reporting()) {
-			$this->_errbit->notify(new Errbit_Errors_Fatal($error['message'], $error['file'], $error['line']));
-		}
-	}
+    public function onException($exception) {
+        $this->_errbit->notify($exception);
 
-	// -- Private Methods
+        if ($this->_prevExceptionHandler) {
+            call_user_func($this->_prevExceptionHandler, $exception);
+        }
+    }
 
-	private function _install($handlers) {
-		if (in_array('error', $handlers)) {
-			set_error_handler(array($this, 'onError'), error_reporting());
-		}
+    public function onShutdown() {
+        if (($error = error_get_last()) && $error['type'] & error_reporting()) {
+            $this->_errbit->notify(new Errbit_Errors_Fatal($error['message'], $error['file'], $error['line']));
+        }
+    }
 
-		if (in_array('exception', $handlers)) {
-			$this->_prevExceptionHandler = set_exception_handler(array($this, 'onException'));
-		}
+    // -- Private Methods
 
-		if (in_array('fatal', $handlers)) {
-			register_shutdown_function(array($this, 'onShutdown'));
-		}
-	}
+    private function _install($handlers) {
+        if (in_array('error', $handlers)) {
+            $this->_prevErrorHandler = set_error_handler(array($this, 'onError'), error_reporting());
+        }
+
+        if (in_array('exception', $handlers)) {
+            $this->_prevExceptionHandler = set_exception_handler(array($this, 'onException'));
+        }
+
+        if (in_array('fatal', $handlers)) {
+            register_shutdown_function(array($this, 'onShutdown'));
+        }
+    }
 }
